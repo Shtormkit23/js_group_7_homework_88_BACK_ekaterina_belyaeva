c24e5985c45a058d1d3e72055914448ff2fd2dcd
maksimus <gorlovmaks23@gmail.com>
2020-11-21 01:54:13 +0600

    Добавила модель для создания постов в базе данных

---
+++ models/Post.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: function() {
+            return !this.image;
+        }
+    },
+    image: {
+        type: String,
+        required: function() {
+            return !this.description;
+        }
+    },
+    user: {
+        type: Schema.Types.ObjectID,
+        ref: "User",
+        required: true
+    },
+    datetime: {
+        type: String,
+        required: true
+    }
+});
+
+const Post = mongoose.model("Post", PostSchema);
+module.exports = Post;
