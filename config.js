5ae6b927b8896ae396db81907a3ef43619d76b3a
maksimus <gorlovmaks23@gmail.com>
2020-11-21 01:53:46 +0600

    Добавила файл для обозначения бд и пр.

---
+++ config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+const rootPath = __dirname;
+
+module.exports = {
+  rootPath,
+  uploadPath: path.join(rootPath, "public/uploads"),
+  db: {
+    name: "blog",
+    url: "mongodb://localhost"
+  }
+};
+
